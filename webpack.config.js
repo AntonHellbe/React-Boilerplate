e470d61e7c950133b97bf2555cc69af78fd10649
AntonHellbe <antonhellbe2@gmail.com>
2017-11-18 15:45:55 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+    entry : './src/app.js',
+    output : {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        },
+        {
+            test: /\.s?css$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+
+    devtool: 'cheap-module-source-map',
+
+    resolve : {
+        extensions : ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './public/'
+    }
+}
