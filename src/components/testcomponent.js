a4058ff91ec869e3cdaa1eefb144051ca2cde2e3
AntonHellbe <antonhellbe2@gmail.com>
2017-11-29 11:51:48 +0100

    global test variables inside test folder, mount / render example tests

--- src/components/testcomponent.js
+++ src/components/testcomponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TestComponent = () => (
-    <div>
+    <div className="foo">
         <h1>Hello</h1>
     </div>
 );
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import TestComponent from '../components/testcomponent';
-import { shallow } from 'enzyme';
 
 test('should assert title for testcomponent', () => {
     const wrapper = shallow(<TestComponent />)
     expect(wrapper.find('h1').text()).toBe('Hello')
+})
+
+test('should render pure HTML', () => {
+    expect(render(<TestComponent />).text()).toEqual('Hello');
+})
+
+test('should mount in a full dom', () => {
+    expect(mount(<TestComponent />).find('.foo').length).toBe(1);
 })
